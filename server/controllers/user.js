ff5f3542316319b656f81739eba81b212483f4ba
Carlos Reyes <cjpiraligua29@gmail.com>
2025-06-27 23:03:41 -0500

    Users System (48) Create Users - To do: fix avatar

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 async function getMe(req, res) {
@@ -28,7 +29,32 @@ async function getUsers(req, res) {
     res.status(200).send(response);
 }
 
+
+async function createUser(req, res){
+    
+    const {password} =req.body;
+
+    const user = new User({ ...req.body, active: false});
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(password, salt);
+    user.password = hashPassword;
+
+    if(req.files.avatar){
+        //To do
+        console.log("Process avatar");
+    }
+
+    try {
+        const userStored = await user.save();
+        return res.status(201).send({ userStored });
+    } catch (error) {
+        return res.status(400).send({ msg: "Error creating user", error });
+    }
+
+}
+
 module.exports = {
     getMe,
-    getUsers
+    getUsers,
+    createUser
 }
