c7fe310c357c5b5d4717b8a7e27073d849db82e7
Carlos Reyes <cjpiraligua29@gmail.com>
2025-06-27 22:01:07 -0500

    Users System (46) Get logged user info

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,17 @@
+const User = require('../models/user');
+
 async function getMe(req, res) {
-    res.status(200).send({msg: "OK"})
+    const {user_id} = req.user;
+
+    const response = await User.findById(user_id);
+
+    if (!response) {
+        return res.status(404).send({msg: "User not found"});
+    }else{
+        res.status(200).send(response)
+    }
+
+
 }
 
 module.exports = {
