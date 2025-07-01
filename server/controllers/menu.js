726f611af25ed9a8101e7cf8acc07cc5504760b3
Carlos Reyes <cjpiraligua29@gmail.com>
2025-06-30 20:45:39 -0500

    Menus System (54) Creating Menu

--- server/controllers/menu.js
+++ server/controllers/menu.js
@@ -1,5 +1,16 @@
 const Menu = require('../models/menu');
 
-module.exports = {
+async function createMenu(req, res) {
+    try {
+        const menu = new Menu(req.body);
+        const menuStored = await menu.save();
+        return res.status(201).send({ menuStored });
+    } catch (err) {
+        return res.status(400).send({ msg: "Error creating menu"});
+    }
+}
 
+
+module.exports = {
+    createMenu
 }
