f2d67cc5bef33837de7ffe96f24aa25ec74a8730
Carlos Reyes <cjpiraligua29@gmail.com>
2025-08-02 00:18:59 -0500

    Courses System (63) Update Course

--- server/controllers/course.js
+++ server/controllers/course.js
@@ -1,3 +1,4 @@
+const course = require('../models/course');
 const Course = require('../models/course');
 const image = require('../utils/image');
 
@@ -32,9 +33,53 @@ async function getCourse(req, res) {
     }
 }
 
+// async function updateCourse(req, res) {
+//     const {id} = req.params
+//     const courseData = req.body;
 
+//     if(req.files.miniature){
+//         const imagePath = image.getFilePath(req.files.miniature);
+//         courseData.miniature = imagePath;
+//     }
+
+//     Course.findByIdAndUpdate({_id: id}, courseData, (err)=>{
+//         if(err){
+//             res.status(400).send({ msg: "Error updating course" }, err);
+//         }else{
+//             res.status(200).send({ msg: "Course updated successfully" }); 
+//         }
+//     })
+// }
+
+async function updateCourse(req, res) {
+    try {
+        const { id } = req.params;
+        const courseData = req.body;
+
+        // Si hay una nueva imagen enviada, obtener su path
+        if (req.files?.miniature) {
+            const imagePath = image.getFilePath(req.files.miniature);
+            courseData.miniature = imagePath;
+        }
+
+        // Actualización usando await y opciones modernas
+        const updatedCourse = await Course.findByIdAndUpdate(id, courseData, {
+            new: true,       // Retorna el documento modificado
+            runValidators: true // Ejecuta validaciones del schema
+        });
+
+        if (!updatedCourse) {
+            return res.status(404).send({ msg: "Course not found" });
+        }
+
+        res.status(200).send({ msg: "Course updated successfully"});
+    } catch (error) {
+        res.status(400).send({ msg: "Error updating course", error: error.message });
+    }
+}
 
 module.exports = { 
     createCourse, 
-    getCourse
+    getCourse,
+    updateCourse
 }
