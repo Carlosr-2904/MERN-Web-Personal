72b766522a209e7617dae047c8b9a0c954ab8731
Carlos Reyes <cjpiraligua29@gmail.com>
2025-08-02 00:25:46 -0500

    Courses System (64) Delete Courses

--- server/controllers/course.js
+++ server/controllers/course.js
@@ -33,23 +33,6 @@ async function getCourse(req, res) {
     }
 }
 
-// async function updateCourse(req, res) {
-//     const {id} = req.params
-//     const courseData = req.body;
-
-//     if(req.files.miniature){
-//         const imagePath = image.getFilePath(req.files.miniature);
-//         courseData.miniature = imagePath;
-//     }
-
-//     Course.findByIdAndUpdate({_id: id}, courseData, (err)=>{
-//         if(err){
-//             res.status(400).send({ msg: "Error updating course" }, err);
-//         }else{
-//             res.status(200).send({ msg: "Course updated successfully" }); 
-//         }
-//     })
-// }
 
 async function updateCourse(req, res) {
     try {
@@ -78,8 +61,25 @@ async function updateCourse(req, res) {
     }
 }
 
+async function deleteCourse(req, res) {
+    const {id}  = req.params;
+
+    try{
+        const deletedCourse = await Course.findByIdAndDelete(id);
+
+        if (!deletedCourse) {
+            return res.status(404).send({ msg: "Course not found" });
+        }
+
+        return res.status(200).send({ msg: "Course deleted successfully" });
+    } catch (error) {
+        return res.status(400).send({ msg: "Error deleting course", error: error.message });
+    }
+}
+
 module.exports = { 
     createCourse, 
     getCourse,
-    updateCourse
+    updateCourse,
+    deleteCourse
 }
