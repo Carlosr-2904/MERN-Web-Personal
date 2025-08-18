0446c490fb3e836bdcf3bdfa5aad8bd3c3753b01
Carlos Reyes <cjpiraligua29@gmail.com>
2025-08-17 23:20:10 -0500

    Newsletter system (74) Register email in newslette

--- server/controllers/newsletter.js
+++ server/controllers/newsletter.js
@@ -1,6 +1,31 @@
 const newsletter = require ('../models/newsletter');
 
 // Functions
+async function suscribeEmail(req, res) {
+    try{
+        const {email} = req.body
+
+        if(!email){
+            return res.status(400).send({msg: "Email is required"});
+        }
+
+        const newsletterData = new newsletter({
+            email: email.toLowerCase()
+        })
+
+        const newsletterStored = await newsletterData.save();
+
+        if (!newsletterStored){
+            return res.status(404).send({msg: "Email could not be subscribed"});
+        }
+
+        return res.status(201).send({msg: "Email subscribed successfully"});
+    }catch (err) {
+        return res.status(500).send({msg: "Error subscribing email", error: err.message});
+    }
+}
 
 
-module.exports = {};
+module.exports = {
+    suscribeEmail
+};
